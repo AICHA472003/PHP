3b2cc0983e80e805d2bea23548708ffdb34e62c4
AICHA472003 <dhiminaicha7@gmail.com>
2026-01-12 10:05:47 +0100

    Add files via upload

---
+++ Chapitre 3 – Boucles for, while, foreach/index.php
@@ -0,0 +1,24 @@
+<html>
+    <body>
+        <?php
+        for($i=1;$i<=10;$i++) {
+            echo "Nombre: $i<br>";
+        }
+        $j=1;
+    while($j<=5) {
+        echo "Itération : $j <br>";
+    $j++;
+    }   
+    $animaux = ["Chat", "Chien", "Lapin"];
+foreach ($animaux as $animal) {
+    echo "Animal : $animal <br>";
+}
+for ($i = 1; $i <= 10; $i++) {
+    if ($i == 5) continue; 
+    if ($i == 8) break;    
+    echo "Valeur : $i <br>";
+}
+      ?>
+        
+    </body>
+</html>
