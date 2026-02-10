d18680328a1bb9eca7ca6e3c087222a343ecbc39
AICHA472003 <dhiminaicha7@gmail.com>
2026-02-10 13:41:00 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h2>Super global</h2>
+     <form action ="page.php" method="post">
+       < label for="">choose color:</label>
+       <input type="text"name=" color" id=""><br>
+       <input type="submit" nam="send" value="value">
+     </form>
+
+     </form>   
+    </body>
+</html>
