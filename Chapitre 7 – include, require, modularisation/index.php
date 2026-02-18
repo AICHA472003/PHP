a04744c7dc185ac2b8f5a99ee7556c5d52a29cb6
AICHA472003 <dhiminaicha7@gmail.com>
2026-02-18 13:34:24 +0000

    Add files via upload

---
+++ Chapitre 7 – include, require, modularisation/index.php
@@ -0,0 +1,11 @@
+<?php
+     include'footer.php';
+     ?>
+     <main>
+              <h2>Page d'accuille</h2>
+        <p>   contenu de  la page principale</p>
+     </main>
+    <?php
+    include'footer.php';
+
+?>
